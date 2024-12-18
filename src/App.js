3500561ab6b87a4f50a20ee28872c8ebab7e3e35
jesslyn-teo <jesslynteowenwen@gmail.com>
2024-12-18 18:47:53 +0800

    feat: adding card components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard.js';
+
+// paths for the image cards
+const imageCards = [
+  { "src": "/img/carnation.png" },
+  { "src": "/img/cherryblossom.png" },
+  { "src": "/img/chrysanthemum.png" },
+  { "src": "/img/iris.png" },
+  { "src": "/img/lily.png" },
+  { "src": "/img/poppy.png" },
+]
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turnCount, setTurnCount] = useState(0);
+
+  // used to duplicate , duplicate and identify the cards
+  const shuffleCards = () => {
+    const shuffledCards = [...imageCards, ...imageCards] 
+      .sort(() => Math.random() - 0.5) 
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards);
+    setTurnCount(0);
+  }
+
+  console.log(cards, turnCount);
+
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   );
 }
