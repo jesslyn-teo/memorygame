988ff93fb1659eb73ae41e6fa0a05c084b528d11
jesslyn-teo <jesslynteowenwen@gmail.com>
2024-12-18 19:31:15 +0800

    feat: card matching logic

--- src/App.js
+++ src/App.js
@@ -1,33 +1,66 @@
-import { useState } from 'react';
-import './App.css';
-import SingleCard from './components/SingleCard.js';
+import { useState, useEffect } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard.js";
 
 // paths for the image cards
 const imageCards = [
-  { "src": "/img/carnation.png" },
-  { "src": "/img/cherryblossom.png" },
-  { "src": "/img/chrysanthemum.png" },
-  { "src": "/img/iris.png" },
-  { "src": "/img/lily.png" },
-  { "src": "/img/poppy.png" },
-]
+  { src: "/img/carnation.png", matched: false },
+  { src: "/img/cherryblossom.png", matched: false },
+  { src: "/img/chrysanthemum.png", matched: false },
+  { src: "/img/iris.png", matched: false },
+  { src: "/img/lily.png", matched: false },
+  { src: "/img/poppy.png", matched: false },
+];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turnCount, setTurnCount] = useState(0);
+  const [firstChoice, setFirstChoice] = useState(null);
+  const [secondChoice, setSecondChoice] = useState(null);
 
   // used to duplicate , duplicate and identify the cards
   const shuffleCards = () => {
-    const shuffledCards = [...imageCards, ...imageCards] 
-      .sort(() => Math.random() - 0.5) 
-      .map((card) => ({ ...card, id: Math.random() }))
+    const shuffledCards = [...imageCards, ...imageCards]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
     setTurnCount(0);
+  };
+
+  // handles image card choice
+  const handleChoice = (card) => {
+    firstChoice ? setSecondChoice(card) : setFirstChoice(card);
+  };
+
+  // comparing cards 
+  useEffect(() => {
+    if(firstChoice && secondChoice) {
+      if(firstChoice.src === secondChoice.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === firstChoice.src) {
+              return {...card, matched: true};
+            } else {
+              return card;
+            }
+          })
+        });
+        resetTurn();
+      } else {
+        resetTurn();
+      }
+    }
+  }, [firstChoice, secondChoice])
+
+  console.log(cards)
+
+  // reset choices and increase turn count 
+  const resetTurn = () => {
+    setFirstChoice(null);
+    setSecondChoice(null);
+    setTurnCount(prevTurns => prevTurns + 1);
   }
-
-  console.log(cards, turnCount);
-
 
   return (
     <div className="App">
@@ -35,8 +68,12 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+        {cards.map((card) => (
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice} 
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,21 @@
 import "./SingleCard.css";
 
-function SingleCard({ card }) {
+function SingleCard({ card, handleChoice }) {
+  // handling the image selection 
+  const handleClick = () => {
+    handleChoice(card);
+  }
+
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src="/img/coverimg.png" alt="card back" />
+        <img
+          className="back"
+          src="/img/coverimg.png"
+          onClick={handleClick}
+          alt="card back"
+        />
       </div>
     </div>
   );
